85703a8b3c2bca5b4b322c1b06007801e4f1b83d
Francisco Manoel <f.manoel.neto312@gmail.com>
2023-04-18 21:59:05 -0300

    Aula 06 / Projeto 02 (Atualizado)

--- Projeto02_tocarsons/main.js
+++ Projeto02_tocarsons/main.js
@@ -1,10 +1,8 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const buttons = document.querySelectorAll('.tecla');
-    for (var i = 0; i < buttons.length; i++) {
-      buttons[i].addEventListener('click', function() {
-        var sound = 'som_' + this.classList[1];
-  
-        document.getElementById(sound).play();
-      });
-    }
+const buttons = document.querySelectorAll('.tecla');
+for (var i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener('click', function () {
+    var sound = 'som_' + this.classList[1];
+
+    document.getElementById(sound).play();
   });
+}
